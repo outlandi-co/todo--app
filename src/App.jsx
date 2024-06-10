3a624255da194444724d4da9dbd0614a845c286b
outlandi-co <outlandico@gmail.com>
2024-06-10 15:35:26 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Todo from './components/Todo';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Todo />
+  </div>
+);
+
+export default App;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+
+import React from 'react';
+
+const Todo = () => {
+  return (
+    <div data-testid="todo-header">
+      <h1 data-testid="todo-h1">To Do List: 0 items pending</h1>
+    </div>
+  );
+};
+
+export default Todo;
